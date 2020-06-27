daf4df36d51e0ecf1d49d43199a31dfc82d9b08b
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-06-27 09:50:23 -0500

    back to normal

--- app/main.py
+++ app/main.py
@@ -5,4 +5,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return render_template("home.html", title = "homepage")
+    #return render_template("home.html", title = "homepage")
+    return "<h1>Welcome to Geeks for Geeks</h1>"
