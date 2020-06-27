3e7cab2b2ce65c96a3c6b1e36d2a2b6aa2d13fe0
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-06-27 09:40:25 -0500

    fixed render_templat

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def home_view():
-    return render_template("home.html")
+    return render_template("home.html", title = "homepage")
