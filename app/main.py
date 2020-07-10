5d091ed9aa74375488b667429bb48281ba24a1ff
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-07-09 23:55:08 -0500

    trying new html

--- app/main.py
+++ app/main.py
@@ -5,5 +5,4 @@ app = Flask(__name__)
 
 @app.route("/")
 def home_view():
-    return render_template("home.html", title = "homepage")
-    #return "<h1>Welcome to Geeks for Geeks</h1>"
+    return render_template("main_page.html", title = "homepage")
