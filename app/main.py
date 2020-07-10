673466af8555ca7c22945cf51a023be373d8a835
Vaishnavi Deshpande <vaishdesh2001@gmail.com>
2020-07-10 12:44:24 -0500

    fixed static url_for

--- app/main.py
+++ app/main.py
@@ -1,8 +1,27 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_from_directory
 
 app = Flask(__name__)
+app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
+app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 
 
 @app.route("/")
+@app.route("/home")
 def home_view():
     return render_template("main_page.html", title = "homepage")
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html", title="About")
+
+
+
+# No caching at all for API endpoints.
+@app.after_request
+def add_header(response):
+    # response.cache_control.no_store = True
+    response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
+    response.headers['Pragma'] = 'no-cache'
+    response.headers['Expires'] = '-1'
+    return response
